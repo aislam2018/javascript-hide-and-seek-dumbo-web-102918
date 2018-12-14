1550b49ed0880d42fe1b26d40d8031af25d370dd
Anik Islam <Anik1606@gmail.com>
2018-12-14 15:17:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  var nested =  document.querySelector("#nested")
+   return nested.querySelector(".target")
+}
+
+function increaseRankBy(n) {
+
+}
+
+function deepestChild() {
+
+let nodes = document.querySelector("#grand-node div")
+    return nodes[nodes.length - 1]
+}
